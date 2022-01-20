74aa5c22111669f18de302ecca8c8281a32bace9
ssim <sjk3037@gmailcom>
2022-01-20 17:02:50 +0900

    remove dbinfo

--- be/server/database/cookie.js
+++ be/server/database/cookie.js
@@ -1,9 +1,9 @@
 const option = {
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'jaen040!',
-  database: 'ssimdb',
+  host: '',
+  port: ,
+  user: '',
+  password: '',
+  database: '',
 };
 
 module.exports = option;
