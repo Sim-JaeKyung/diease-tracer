88b67d9d47a138ba2cc7da5b86fcfb858c5c9c9a
ssim <sjk3037@gmailcom>
2022-01-20 16:32:55 +0900

    gitIgnorePatch

--- be/server/database/cookie.js
+++ be/server/database/cookie.js
@@ -1,9 +1,9 @@
 const option = {
-  host: '',
-  port: ,
-  user: '',
-  password: '!',
-  database: '',
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'jaen040!',
+  database: 'ssimdb',
 };
 
 module.exports = option;
